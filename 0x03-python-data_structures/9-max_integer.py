edc37a2f29828819eec7dd7caf6a224f2d946dba
MutaiAllan <allankiprop175@gmail.com>
2023-01-09 13:54:50 -0800

    Maximum

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    maximum = my_list[0]
+    for i in range(len(my_list)):
+        if maximum < my_list[i]:
+            maximum = my_list[i]
+
+    return maximum
