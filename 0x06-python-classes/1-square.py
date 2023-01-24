1048cbfe01299c491fcc35b613d2cb708824b059
MutaiAllan <allankiprop175@gmail.com>
2023-01-24 08:30:18 -0800

    added task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a class square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Initializes a new square
+
+        Args: size - size of the square defined"""
+        self.__size = size
