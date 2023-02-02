4673ee90641e4b4c7f79134141e435c49e931b58
MutaiAllan <allankiprop175@gmail.com>
2023-02-02 10:43:41 -0800

    Solved task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+"""Defines a function that prints a square."""
+
+
+def print_square(size):
+    """Prints a square with # character.
+
+    Args:
+        size: Integer of size of sides.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size < 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
