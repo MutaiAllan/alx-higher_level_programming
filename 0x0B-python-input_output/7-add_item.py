bfbdcdf3641f939b9d63be84e70b398ae1c2c5c7
MutaiAllan <allankiprop175@gmail.com>
2023-02-12 05:20:13 -0800

    Solved task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a function that adds all arguments and save them in a file."""
+import sys
+import json
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        arg_lst = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        arg_lst = []
+
+    i = 1
+
+    while i < len(sys.argv):
+        arg_lst.append(sys.argv[i])
+        i += 1
+
+    save_to_json_file(arg_lst, "add_item.json")
