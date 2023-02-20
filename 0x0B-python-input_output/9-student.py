a22048166f25fe6dc483ae333cb76f1c13af0bb5
MutaiAllan <allankiprop175@gmail.com>
2023-02-20 11:43:35 -0800

    Solved task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pyhton3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new student.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Getsa dictionary representation of the student."""
+        return self.__dict__
