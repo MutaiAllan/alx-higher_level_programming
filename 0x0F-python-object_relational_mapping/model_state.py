a547fe214e9468e4db58bcacd5b612b88da1d4e9
MutaiAllan <allankiprop175@gmail.com>
2023-04-21 04:02:22 -0700

    Added description

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ Inherits from case class. """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
