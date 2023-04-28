7e95e59d112e74613a50ab385b807085ac24fd85
MutaiAllan <allankiprop175@gmail.com>
2023-04-27 17:00:11 -0700

    Solved task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Defines the function find_peak """
+
+def find_peak(list_of_integers):
+    """ Finds a peak in an unsorted list of integers. """
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    lower = 0
+    size = len(list_of_integers)
+    mid = size // 2
+    if size == 1:
+        return list_of_integers[0]
+    if size == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
