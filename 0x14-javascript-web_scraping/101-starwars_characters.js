3898dac73b869b1b07e6e8733c692da0ad71b300
MutaiAllan <allankiprop175@gmail.com>
2023-05-24 22:49:26 +0000

    Solved task 8

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+let characters = [];
+
+req(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  characters = data.characters;
+  getCharacters(0);
+});
+
+const getCharacters = (index) => {
+  if (index === characters.length) {
+    return;
+  }
+
+  req(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const chardata = JSON.parse(body);
+    console.log(chardata.name);
+    getCharacters(index + 1);
+  });
+};
